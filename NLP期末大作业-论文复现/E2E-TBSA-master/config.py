d5a086005cac3fd05165de63f17bddf7a85e7605
SerendipityFanan <2737035698@qq.com>
2024-12-14 22:28:40 +0800

    change

--- NLP期末大作业-论文复现/E2E-TBSA-master/config.py
+++ NLP期末大作业-论文复现/E2E-TBSA-master/config.py
@@ -14,6 +14,57 @@ laptop14 = {
     "epsilon": 0.5
 }
 
+rest14 = {
+    "dim_w": 768,  # BERT 嵌入维度
+    "dim_ote_h": 50,
+    "dim_ts_h": 50,
+    "input_win": 3,
+    "stm_win": 3,
+    "optimizer": "adam",
+    "n_epoch": 40,
+    "dropout": 0.5,
+    "tagging_schema": "BIEOS",
+    "lr_decay": 0.05,
+    "use_char": 0,
+    "dynet_seed": 1314159,
+    "random_seed": 1234,
+    "epsilon": 0.5
+}
+
+rest15 = {
+    "dim_w": 768,  # BERT 嵌入维度
+    "dim_ote_h": 50,
+    "dim_ts_h": 50,
+    "input_win": 3,
+    "stm_win": 3,
+    "optimizer": "adam",
+    "n_epoch": 40,
+    "dropout": 0.5,
+    "tagging_schema": "BIEOS",
+    "lr_decay": 0.05,
+    "use_char": 0,
+    "dynet_seed": 1314159,
+    "random_seed": 1234,
+    "epsilon": 0.5
+}
+
+rest16 = {
+    "dim_w": 768,  # BERT 嵌入维度
+    "dim_ote_h": 50,
+    "dim_ts_h": 50,
+    "input_win": 3,
+    "stm_win": 3,
+    "optimizer": "adam",
+    "n_epoch": 40,
+    "dropout": 0.5,
+    "tagging_schema": "BIEOS",
+    "lr_decay": 0.05,
+    "use_char": 0,
+    "dynet_seed": 1314159,
+    "random_seed": 1234,
+    "epsilon": 0.5
+}
+
 rest_total = {
     "dim_ote_h": 50,
     "dim_ts_h": 50,
